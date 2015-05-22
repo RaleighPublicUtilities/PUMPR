2ddc1fd6be107e55f7d74127065162d47d14234d
cwhite911 <smortopahri@gmail.com>
2015-05-22 15:33:37 -0400

    simplified file upload

--- server/api/document/index.js
+++ server/api/document/index.js
@@ -9,7 +9,7 @@ var express = require('express'),
     fs = require('fs');
 
 var router = express.Router();
-
+var currentFile;
 
 //Route Specific Middleware
 //Handles file uploads
@@ -22,41 +22,15 @@ router.use(multer({
   //Sets the file name from optoins
   rename: function (fieldname, filename){
     console.log('File Name: ' + filename);
+    currentFile = filename
     return filename;
   },
-  //Creates new directory
-  onFileUploadStart: function(file, req, res){
-    var folder = file.path.split('.')[0].split('-')[0];
+  changeDest: function(dest, req, res){
+    var folder = dest + '/' + currentFile.split('-')[0];
     fs.mkdir(folder, function(err){
       if (err) return;
     });
-  },
-  //Add new file to proper directory
-  onFileUploadComplete: function (file) {
-    var folder = file.path.split('.')[0].split('-')[0];
-    //Creates Read Stream to uploaded file
-      var source = fs.createReadStream(path.join(folder, file.name));
-    //Sets the destination and creates write stream to that loacation
-      var dest = fs.createWriteStream(folder + '/' + file.name);
-    //Pipes  the source data to the destination
-      source.pipe(dest);
-      //Report data transfer
-      source.on('data', function(chunk) {
-          console.log('got %d bytes of data', chunk.length);
-        });
-        //Deletes original after readstream closes
-        source.on('end', function(){
-          console.log('File Copied:' + file.name);
-          //Deletes Original
-          fs.unlink(file.path, function (err){
-            if (err) throw err;
-            console.log('successfully deleted ' + file.path);
-          });
-        });
-        source.on('error', function(err){
-          console.log(err);
-          if(err) throw err;
-        });
+    return folder;
   },
   onError: function (error, next) {
     console.log(error);
