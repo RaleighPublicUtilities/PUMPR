4d5caca39c1eb403a6c97f6dd4b74dbeb83c7e8e
cwhite911 <smortopahri@gmail.com>
2015-06-16 16:14:24 -0400

    Added project table

--- client/app/project/project.controller.js
+++ client/app/project/project.controller.js
@@ -15,6 +15,10 @@ angular.module('pumprApp')
     i = 0,
     root;
 
+    $scope.tabs = [
+      { title:'Dynamic Title 1', content:'Dynamic content 1' },
+      { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+    ];
 /*hell
 dsfs
 */
@@ -286,6 +290,9 @@ function removeEmptyFields (data) {
     return data;
   }
 
+function buildTree(){
+
+}
 
 
 
