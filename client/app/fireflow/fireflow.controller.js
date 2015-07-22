dfc24e8d62679ed4fcd252d3b2ba3c333ff56d35
cwhite911 <smortopahri@gmail.com>
2015-07-22 10:44:22 -0400

    added geoip and baselayers

--- client/app/fireflow/fireflow.controller.js
+++ client/app/fireflow/fireflow.controller.js
@@ -1,6 +1,31 @@
 'use strict';
 
 angular.module('pumprApp')
-  .controller('FireflowCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('FireflowCtrl', function ($scope, Auth, mapLayers, leafletData) {
+    //Make map height 100%
+    angular.element('body').find('div').addClass('fullScreen');
+
+    $scope.agsToken = Auth.getAgolToken();
+    $scope.searchStatus = false;
+    mapLayers.overlays.water.visible = true;
+    mapLayers.overlays.water.layerParams = {
+        token: $scope.agsToken
+    },
+    mapLayers.overlays.sewer.visible = false;
+    mapLayers.overlays.reuse.visible = false;
+
+    angular.extend($scope, {
+      center: {
+        lat: 35.77882840327371,
+        lng: -78.63945007324219,
+        zoom: 13
+      },
+      layers: mapLayers
+    });
+
+    leafletData.getMap('fireflow-map').then(function(map) {
+      map.locate({setView: true, maxZoom: 17});
+    });
+
+
+  });//End Controller
