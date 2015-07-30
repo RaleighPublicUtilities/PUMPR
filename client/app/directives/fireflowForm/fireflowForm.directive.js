95d2c6737382b4f4453cec76834c96e208268a56
cwhite911 <smortopahri@gmail.com>
2015-07-30 14:33:27 -0400

    Change esri-leaflet to fixed version 1.0.0-rc.8

--- client/app/directives/fireflowForm/fireflowForm.directive.js
+++ client/app/directives/fireflowForm/fireflowForm.directive.js
@@ -1,11 +1,33 @@
 'use strict';
 
 angular.module('pumprApp')
-  .directive('fireflowForm', function () {
+  .directive('fireflowForm', function (agsServer) {
     return {
       templateUrl: 'app/directives/fireflowForm/fireflowForm.html',
-      restrict: 'EA',
-      link: function (scope, element, attrs) {
+      transclude: true,
+      restrict: 'E',
+      scope: {
+        flowData: '='
+      },
+      link: function (scope) {
+        var options = {
+          layer: 'FireFlow',
+          actions: 'query',
+          params: {
+            f: 'json',
+            where: '1=1',
+            outFields: '*',
+            returnGeometry: false
+          }
+        }
+        agsServer.ptFs.request(options)
+          .then(function(res){
+            console.log(res);
+          })
+          .catch(function(err){
+            console.log(err);
+          });
+
       }
     };
   });
