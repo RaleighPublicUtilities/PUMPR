d3b8c3d7313a0a2e35e14f42224bfe374288bbe8
cwhite911 <smortopahri@gmail.com>
2015-08-03 15:03:39 -0400

    Auto add address and user

--- client/app/directives/fireflowForm/fireflowForm.directive.js
+++ client/app/directives/fireflowForm/fireflowForm.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('pumprApp')
-  .directive('fireflowForm', function (fireflowFactory) {
+  .directive('fireflowForm', function (fireflowFactory, agsServer, Auth) {
     return {
       templateUrl: 'app/directives/fireflowForm/fireflowForm.html',
       transclude: true,
@@ -10,17 +10,54 @@ angular.module('pumprApp')
         hydrants: '='
       },
       link: function (scope) {
+        var user = Auth.getCurrentUser();
         scope.flowData = {};
         scope.$watchCollection('hydrants', function(){
           if (Array.isArray(scope.hydrants) && scope.hydrants.length === 2){
             angular.extend(scope.flowData, {
               TESTID: 1,
               TESTFACILITYID: scope.hydrants[0].message.split(':')[1].trim(),
-              FLOWFACILITYID: scope.hydrants[1].message.split(':')[1].trim()
+              FLOWFACILITYID: scope.hydrants[1].message.split(':')[1].trim(),
+              APPLICANTNAME: user.name,
+              CONTACTEMAIL: user.email
             });
           }
         });
 
+        scope.findAddress = function (typed){
+          var typed = typed.toUpperCase();
+          var options = {
+            layer: 'Addresses',
+            geojson: false,
+            actions: 'query',
+            params: {
+              f: 'json',
+              outFields: 'ADDRESSU, CITY, STATE, ZIP',
+              where: "ADDRESSU like '%" +typed + "%'",
+              returnGeometry: false,
+              orderByFields: 'ADDRESSU ASC'
+            }
+          };
+          scope.addressPromise = agsServer.addressesMs.request(options);
+          return scope.addressPromise
+            .then(function(data){
+              if (data.error){
+                console.log(data.error);
+              }
+              else {
+                return data.features.map(function(item){
+                  return item.attributes.ADDRESSU + ', ' +  item.attributes.CITY + ', ' +  item.attributes.STATE + ', ' +  item.attributes.ZIP;
+              });
+              }
+            })
+            .catch(function(err){
+              console.log(err);
+            });
+        };
+
+        scope.searchControl = function (item){
+          scope.flowData.ADDRESS = item;
+        }
 
         // fireflowFactory.getForm().then(function(res){
         //     console.log(res);
