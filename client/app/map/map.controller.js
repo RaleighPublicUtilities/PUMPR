2295542813292ccf9a7f4c9add80d2aada1bda0f
cwhite911 <smortopahri@gmail.com>
2015-09-03 16:04:54 -0400

    Set base map view

--- client/app/map/map.controller.js
+++ client/app/map/map.controller.js
@@ -1,6 +1,34 @@
 'use strict';
 
 angular.module('pumprApp')
-  .controller('MapCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('MapCtrl', function ($scope, Auth, mapLayers, leafletData) {
+    //Make map height 100%
+    angular.element('body').find('div').addClass('fullScreen');
+    //Get token from ArcGIS Server
+    $scope.agsToken = Auth.getAgolToken();
+    $scope.searchStatus = false;
+    mapLayers.overlays.water.visible = true;
+    mapLayers.overlays.water.layerParams = {
+        token: $scope.agsToken
+    },
+    mapLayers.overlays.sewer.visible = true;
+    mapLayers.overlays.reuse.visible = true;
+
+    //Set default map settings
+    angular.extend($scope, {
+      center: {
+        lat: 35.77882840327371,
+        lng: -78.63945007324219,
+        zoom: 13
+      },
+      layers: mapLayers,
+      events: {
+        map: {
+          enable: ['click'],
+          logic: 'emit'
+        }
+      },
+      // markers: fireflowFactory.getLog()
+    });
+
   });
