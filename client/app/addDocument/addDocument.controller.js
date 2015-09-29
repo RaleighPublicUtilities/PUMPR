50093d7f6dbd5ce3d4326dc44a79a9bc6523ba41
cwhite911 <smortopahri@gmail.com>
2015-09-29 16:21:31 -0400

    Restructured code style

--- client/app/addDocument/addDocument.controller.js
+++ client/app/addDocument/addDocument.controller.js
@@ -1,68 +1,81 @@
-'use strict';
+/**
+ * AddDocument Controller
+ * @namespace Controller
+ * @desc Controls addDocument view
+ */
 
-angular.module('pumprApp')
-  .controller('AddDocumentCtrl', ['$scope', 'search', '$location',
-    function ($scope, search, $location) {
-      //Set root scope as scope
+(function(){
+  'use strict';
 
-      var path = $location.path();
-      $scope.project = {};
-      $scope.projectname;
+  angular
+    .module('pumprApp')
+    .controller('AddDocumentCtrl', AddDocumentCtrl);
 
-      $scope.reload = function(){
+    AddDocumentCtrl.$inject = ['$scope', 'search', '$location'];
+
+  function AddDocumentCtrl($scope, search, $location) {
+      var self = this;
+
+      self.path = $location.path();
+      self.projectid;
+      self.reload = reload;
+
+      activate();
+
+      function activate() {
+        var options;
+        if (self.path.split('/').length === 3){
+          self.projectid = self.path.split('/')[2];
+          options = {
+            layer: 'RPUD.PTK_DOCUMENTS',
+            actions: 'query',
+            params: {
+              f: 'json',
+              where: 'PROJECTID = ' + self.projectid,
+              outFields: '*',
+              orderByFields: 'DOCID ASC',
+              returnGeometry: false
+            }
+          };
+
+          $scope.projectPromise = search.display(self.projectid)
+            .then(prepareForm)
+            .catch(function(error){
+              $scope.projectError = true;
+          });
+        }
+      }
+
+      function prepareForm(data) {
+        var project = data[0];
+        data = data[1];
+        //If no project documents have been added get project name from db
+        if (!data.length){
+          $scope.projectname = project.features[0].properties['Project Name'];
+          //Prepare document to be added
+          $scope.project = [{
+            new: true,
+            attributes: {
+              PROJECTID: self.projectid,
+              PROJECTNAME: project.features[0].properties['Project Name'],
+              DEVPLANID: project.features[0].properties['Development Plan ID']
+            }
+          }];
+        }
+        //If project documents have been added
+        else {
+          $scope.projectname = project.features[0].properties['Project Name'];
+          $scope.project = data;
+        }
+        //Activates table view
+        $scope.searchStatus = true;
+        $scope.project_docs = true;
+      }
+
+      function reload() {
         document.location.reload();
       }
 
-//Checks if project is in path and set view to project
-      if (path.split('/').length === 3){
-        var projectid = path.split('/')[2];
-        var options = {
-          layer: 'RPUD.PTK_DOCUMENTS',
-          actions: 'query',
-          params: {
-            f: 'json',
-            where: 'PROJECTID = ' + projectid,
-            outFields: '*',
-            orderByFields: 'DOCID ASC',
-            returnGeometry: false
-          }
-        };
+    }
 
-
-        $scope.projectPromise = search.display(projectid).then(function(data){
-          var project = data[0];
-
-          data = data[1];
-
-          //If no project documents have been added get project name from db
-          if (!data.length){
-              $scope.projectname = project.features[0].properties['Project Name'];
-            //Prepare document to be added
-            $scope.project = [{
-              new: true,
-              attributes: {
-                PROJECTID: projectid,
-                PROJECTNAME: project.features[0].properties['Project Name'],
-                DEVPLANID: project.features[0].properties['Development Plan ID']
-              }
-            }];
-
-          }
-          //If project documents have been added
-          else {
-            $scope.projectname = project.features[0].properties['Project Name'];
-            $scope.project = data;
-          }
-          //Activates table view
-          $scope.searchStatus = true;
-          $scope.project_docs = true;
-        })
-        .catch(function(error){
-          $scope.projectError = true;
-        });
-
-      }
-
-
-
-    }]);
+})();
