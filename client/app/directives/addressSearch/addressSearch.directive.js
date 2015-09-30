73483111c6a0a19c0e81e3ece030b5fad1082c7b
cwhite911 <smortopahri@gmail.com>
2015-09-30 10:36:30 -0400

    starting to update address search

--- client/app/directives/addressSearch/addressSearch.directive.js
+++ client/app/directives/addressSearch/addressSearch.directive.js
@@ -1,53 +1,62 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('pumprApp')
-  .directive('addressSearch', function (agsServer) {
-    return {
+  angular
+    .module('pumprApp')
+    .directive('addressSearch', addressSearch);
+
+    addressSearch.$inject = ['agsServer'];
+
+  function addressSearch(agsServer) {
+    var directive = {
       templateUrl: 'app/directives/addressSearch/addressSearch.html',
       transclude: true,
       restrict: 'E',
       scope: {
         value: '=ngModel'
       },
-      link: function (scope) {
-
-        scope.findAddress = function (typed){
-          console.log(typed)
-          var typed = typed.toUpperCase();
-          var options = {
-            layer: 'Addresses',
-            geojson: false,
-            actions: 'query',
-            timeout: 30000,
-            params: {
-              f: 'json',
-              outFields: 'ADDRESSU, CITY, STATE, ZIP',
-              where: "ADDRESSU like '%" +typed + "%'",
-              returnGeometry: false,
-              orderByFields: 'ADDRESSU ASC'
-            }
-          };
-          scope.addressPromise = agsServer.addressesMs.request(options);
-          return scope.addressPromise
-            .then(function(data){
-              if (data.error){
-                console.log(data.error);
-              }
-              else {
-                return data.features.map(function(item){
-                  return item.attributes.ADDRESSU + ', ' +  item.attributes.CITY + ', ' +  item.attributes.STATE + ', ' +  item.attributes.ZIP;
-              });
-              }
-            })
-            .catch(function(err){
-              console.log(err);
-            });
-        };
-
-        scope.searchControl = function (item){
-          scope.value = item;
-        }
-
-      }
+      link: link
     };
-  });
+    return directive;
+
+    function link(scope) {
+
+      scope.findAddress = function (typed){
+        var typed = typed.toUpperCase();
+        var options = {
+          layer: 'Addresses',
+          geojson: false,
+          actions: 'query',
+          timeout: 30000,
+          params: {
+            f: 'json',
+            outFields: 'ADDRESSU, CITY, STATE, ZIP',
+            where: "ADDRESSU like '%" +typed + "%'",
+            returnGeometry: false,
+            orderByFields: 'ADDRESSU ASC'
+          }
+        };
+        scope.addressPromise = agsServer.addressesMs.request(options);
+        return scope.addressPromise
+          .then(function(data){
+            if (data.error){
+              console.log(data.error);
+            }
+            else {
+              return data.features.map(function(item){
+                return item.attributes.ADDRESSU + ', ' +  item.attributes.CITY + ', ' +  item.attributes.STATE + ', ' +  item.attributes.ZIP;
+            });
+            }
+          })
+          .catch(function(err){
+            console.log(err);
+          });
+      };
+
+      scope.searchControl = function (item){
+        scope.value = item;
+      }
+
+    }
+  }
+})();
